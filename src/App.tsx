4241cd24456673fcfd125286d53f4d58ff647264
Dorvad <dor1612@gmail.com>
2026-03-14 03:19:44 +0200

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { HashRouter, Routes, Route } from 'react-router-dom'
+import { Layout } from './components/Layout'
+import { ProtectedRoute } from './components/ProtectedRoute'
+import HomePage from './pages/HomePage'
+import RequestPage from './pages/RequestPage'
+import SchedulePage from './pages/SchedulePage'
+import CancelPage from './pages/CancelPage'
+import AdminLoginPage from './pages/AdminLoginPage'
+import AdminPage from './pages/AdminPage'
+
+export default function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Layout><HomePage /></Layout>} />
+        <Route path="/request" element={<Layout><RequestPage /></Layout>} />
+        <Route path="/schedule" element={<Layout><SchedulePage /></Layout>} />
+        <Route path="/cancel" element={<Layout><CancelPage /></Layout>} />
+        <Route path="/admin/login" element={<Layout><AdminLoginPage /></Layout>} />
+        <Route path="/admin" element={
+          <Layout>
+            <ProtectedRoute>
+              <AdminPage />
+            </ProtectedRoute>
+          </Layout>
+        } />
+      </Routes>
+    </HashRouter>
+  )
+}
